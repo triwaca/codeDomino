4f3822f88043aa6dfb5e6329810b12201c771682
Daniel Almeida Chagas <daniel.helptech@gmail.com>
2025-04-22 18:47:45 -0300

    Update stepMotorTest.ino

--- sketches/stepMotorTest/stepMotorTest.ino
+++ sketches/stepMotorTest/stepMotorTest.ino
@@ -8,16 +8,16 @@
  * 
  */
 
-int pins()= {4, 5, 6, 7};
+int pins[]= {4, 5, 6, 7};
 
 int delayTime = 4;
 int coilControl = 0;
 
 void setup(){
-  pinMode(pins(0), OUTPUT);
-  pinMode(pins(1), OUTPUT);
-  pinMode(pins(2), OUTPUT);
-  pinMode(pins(3), OUTPUT);
+  pinMode(pins[0], OUTPUT);
+  pinMode(pins[1], OUTPUT);
+  pinMode(pins[2], OUTPUT);
+  pinMode(pins[3], OUTPUT);
 }
 
 void loop(){
@@ -31,8 +31,8 @@ void runMotor(){
   if(nextCoil1>3) nextCoil1 = 0;
   int nextCoil2 = nextCoil1 + 1;
   if(nextCoil2>3) nextCoil2 = 0;
-  digitalWrite(coilControl, LOW);
-  digitalWrite(nextCoil1, HIGH);
-  digitalWrite(nextCoil2, HIGH);
+  digitalWrite(pins[coilControl], LOW);
+  digitalWrite(pins[nextCoil1], HIGH);
+  digitalWrite(pins[nextCoil2], HIGH);
   delay(delayTime);
 }
